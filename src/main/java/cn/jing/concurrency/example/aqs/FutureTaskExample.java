c115164b8d2a766a188af99e8a8aed6a56245c94
梁竞 <1184106223@qq.com>
2018-06-12 21:17:47 +0800

    J.U.C--FutureTask、

---
+++ src/main/java/cn/jing/concurrency/example/aqs/FutureTaskExample.java
@@ -0,0 +1,36 @@
+package cn.jing.concurrency.example.aqs;
+
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * function:使用Callable+FutureTask获取执行结果
+ * 
+ * @author liangjing
+ *
+ */
+@Slf4j
+public class FutureTaskExample {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+				log.info("do something in callable");
+				Thread.sleep(5000);
+				return "Done";
+			}
+		});
+
+		new Thread(futureTask).start();
+		log.info("do something in main");
+		Thread.sleep(1000);
+		String result = futureTask.get();
+		log.info("result:{}", result);
+	}
+}
