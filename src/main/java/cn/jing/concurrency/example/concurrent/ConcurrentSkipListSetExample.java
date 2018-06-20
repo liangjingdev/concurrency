e6a8d954916c270ea954353a06ca2af97bc8f3a0
梁竞 <1184106223@qq.com>
2018-06-20 10:16:20 +0800

    Java多线程之并发容器--ConcurrentSkipListSet、

---
+++ src/main/java/cn/jing/concurrency/example/concurrent/ConcurrentSkipListSetExample.java
@@ -0,0 +1,70 @@
+package cn.jing.concurrency.example.concurrent;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import cn.jing.concurrency.annotations.NotThreadSafe;
+import cn.jing.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * function:并发容器
+ * 
+ * @author liangjing
+ *
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListSetExample {
+
+	// 请求总数
+	public static int clientTotal = 5000;
+	// 同时并发执行的线程数
+	public static int threadTotal = 50;
+
+	public static Set<Integer> set = new ConcurrentSkipListSet<>();
+
+	public static void main(String[] args) throws InterruptedException {
+		// 创建线程池
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		// 信号量（定义同时并发执行的线程数）
+		final Semaphore semaphore = new Semaphore(threadTotal);
+		// 闭锁
+		final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+		// 模拟并发请求
+		for (int i = 0; i < clientTotal; i++) {
+			final int count = i;
+			executorService.execute(() -> {
+				try {
+					// 请求一个信号，如果信号量小于clientTotal，则阻塞
+					semaphore.acquire();
+					update(count);
+					// 释放一个信号
+					semaphore.release();
+				} catch (InterruptedException e) {
+					log.error("exception", e);
+				}
+				// countDown：减一
+				countDownLatch.countDown();
+			});
+		}
+		// 阻塞直到countDown的次数为threadTotal
+		countDownLatch.await();
+		// 关闭线程池
+		executorService.shutdown();
+		// 如果是线程安全的话，那么此处打印出来的长度应该是5000
+		log.info("size:{}", set.size());
+	}
+
+	private static void update(int i) {
+		set.add(i);
+	}
+}
