7aae6192ec58e0a8fb00460275103c6236388182
梁竞 <1184106223@qq.com>
2018-07-04 22:38:39 +0800

    Java并发编程之线程安全手段--同步容器类、

---
+++ src/main/java/cn/jing/concurrency/example/syncContainer/CollectionsExample2.java
@@ -0,0 +1,69 @@
+package cn.jing.concurrency.example.syncContainer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import cn.jing.concurrency.annotations.NotThreadSafe;
+import cn.jing.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * function:利用Collections的静态工厂方法所创建的同步容器类
+ * 
+ * @author liangjing
+ *
+ */
+@Slf4j
+@ThreadSafe
+public class CollectionsExample2 {
+
+	// 请求总数
+	public static int clientTotal = 5000;
+	// 同时并发执行的线程数
+	public static int threadTotal = 50;
+
+	public static Set<Integer> list = Collections.synchronizedSet(new HashSet<>());
+
+	public static void main(String[] args) throws InterruptedException {
+		// 创建线程池
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		// 信号量（定义同时并发执行的线程数）
+		final Semaphore semaphore = new Semaphore(threadTotal);
+		// 闭锁
+		final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+		// 模拟并发请求
+		for (int i = 0; i < clientTotal; i++) {
+			final int count = i;
+			executorService.execute(() -> {
+				try {
+					// 请求一个信号，如果信号量小于clientTotal，则阻塞
+					semaphore.acquire();
+					update(count);
+					// 释放一个信号
+					semaphore.release();
+				} catch (InterruptedException e) {
+					log.error("exception", e);
+				}
+				// countDown：减一
+				countDownLatch.countDown();
+			});
+		}
+		// 阻塞直到countDown的次数为threadTotal
+		countDownLatch.await();
+		// 关闭线程池
+		executorService.shutdown();
+		// 如果是线程安全的话，那么此处打印出来的长度应该是5000
+		log.info("size:{}", list.size());
+	}
+
+	private static void update(int i) {
+		list.add(i);
+	}
+}
